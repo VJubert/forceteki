4d163367962e0c8bd384c5d5cfc1961313a7c5d7
Valentin Jubert <valentin.jubert@ideine.fr>
2024-10-05 00:42:17 +0200

    lothal insurgent

--- server/game/cards/01_SOR/units/LothalInsurgent.ts
+++ server/game/cards/01_SOR/units/LothalInsurgent.ts
@@ -2,11 +2,11 @@ import AbilityHelper from '../../../AbilityHelper';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
 import { Location } from '../../../core/Constants';
 
-export default class BountyHunterCrew extends NonLeaderUnitCard {
+export default class LothalInsurgent extends NonLeaderUnitCard {
     protected override getImplementationId () {
         return {
-            id: '3684950815',
-            internalName: 'bounty-hunter-crew'
+            id: '1880931426',
+            internalName: 'lothal-insurgent'
         };
     }
 
@@ -14,18 +14,16 @@ export default class BountyHunterCrew extends NonLeaderUnitCard {
         this.addWhenPlayedAbility({
             title: 'Return an event from a discard pile',
             targetResolver: {
-                immediateEffect: AbilityHelper.immediateEffects.conditional({
-                    condition: context => ,
-                    onFalse: AbilityHelper.immediateEffects.noAction(),
-                    onTrue: AbilityHelper.immediateEffects.sequential([
-                        AbilityHelper.immediateEffects.draw(context => ({ target: context.source.controller.opponent })),
-                        AbilityHelper.immediateEffects.discard(),
-                    ])
-
-                })
+                // immediateEffect: AbilityHelper.immediateEffects.conditional({
+                //     // condition: context => ,
+                //     onFalse: AbilityHelper.immediateEffects.noAction(),
+                //     onTrue: AbilityHelper.immediateEffects.sequential([
+                //         AbilityHelper.immediateEffects.draw(context => ({ target: context.source.controller.opponent })),
+                //         // AbilityHelper.immediateEffects.discard(),
+                //     ])
             }
         });
     }
 }
 
-BountyHunterCrew.implemented = true;
+LothalInsurgent.implemented = true;
