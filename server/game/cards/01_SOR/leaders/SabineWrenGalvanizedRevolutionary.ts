7a133f54004af75d154241305f5d48429fa5e5f5
Valentin Jubert <val_jub@live.fr>
2024-10-07 13:30:58 +0200

    sabine

--- server/game/cards/01_SOR/leaders/SabineWrenGalvanizedRevolutionary.ts
+++ server/game/cards/01_SOR/leaders/SabineWrenGalvanizedRevolutionary.ts
@@ -1,6 +1,5 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
-import { RelativePlayer } from '../../../core/Constants';
 
 export default class SabineWrenGalvanizedRevolutionary extends LeaderUnitCard {
     protected override getImplementationId () {
@@ -13,27 +12,21 @@ export default class SabineWrenGalvanizedRevolutionary extends LeaderUnitCard {
     protected override setupLeaderSideAbilities () {
         this.addActionAbility({
             title: 'Deal 1 damage to each base',
-            cost: [AbilityHelper.costs.exhaustSelf()],
-            targetResolver: {
-                controller: RelativePlayer.Self,
-                immediateEffect: AbilityHelper.immediateEffects.simultaneous([
-                    AbilityHelper.immediateEffects.damage((context) => ({ amount: 1, target: context.source.controller.base })),
-                    AbilityHelper.immediateEffects.damage((context) => ({ amount: 1, target: context.source.controller.opponent.base }))
-                ])
-            }
+            cost: AbilityHelper.costs.exhaustSelf(),
+            immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                AbilityHelper.immediateEffects.damage((context) => ({ amount: 1, target: context.source.controller.base })),
+                AbilityHelper.immediateEffects.damage((context) => ({ amount: 1, target: context.source.controller.opponent.base }))
+            ])
         });
     }
 
     protected override setupLeaderUnitSideAbilities () {
         this.addOnAttackAbility({
             title: 'Deal 1 damage to each base',
-            optional: true,
-            targetResolver: {
-                immediateEffect: AbilityHelper.immediateEffects.simultaneous([
-                    AbilityHelper.immediateEffects.damage((context) => ({ amount: 1, target: context.source.controller.base })),
-                    AbilityHelper.immediateEffects.damage((context) => ({ amount: 1, target: context.source.controller.opponent.base }))
-                ])
-            }
+            immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                AbilityHelper.immediateEffects.damage((context) => ({ amount: 1, target: context.source.controller.base })),
+                AbilityHelper.immediateEffects.damage((context) => ({ amount: 1, target: context.source.controller.opponent.base }))
+            ])
         });
     }
 }
--- test/server/cards/01_SOR/leaders/SabineWrenGalvanizedRevolutionary.spec.js
+++ test/server/cards/01_SOR/leaders/SabineWrenGalvanizedRevolutionary.spec.js
@@ -14,7 +14,6 @@ describe('Sabine Wren, Galvanized Revolutionary', function () {
             it('should damage each base', function () {
                 this.player1.clickCard(this.sabineWren);
                 this.player1.clickPrompt('Deal 1 damage to each base');
-                this.player1.clickPrompt('Deal 1 damage to each base');
                 expect(this.sabineWren.exhausted).toBe(true);
                 expect(this.p1Base.damage).toBe(1);
                 expect(this.p2Base.damage).toBe(1);
@@ -22,36 +21,42 @@ describe('Sabine Wren, Galvanized Revolutionary', function () {
             });
         });
 
-        // describe('Sabine\'s deployed ability', function () {
-        //     beforeEach(function () {
-        //         this.setupTest({
-        //             phase: 'action',
-        //             player1: {
-        //                 leader: { card: 'sabine-wren#galvanized-revolutionary', deployed: true }
-        //             },
-        //             player2: {
-        //                 groundArena: ['battlefield-marine']
-        //             }
-        //         });
-        //     });
-        //
-        //     it('should damage each base', function () {
-        //         this.player1.clickCard(this.sabineWren);
-        //         expect(this.player1).toBeAbleToSelectExactly([this.p2Base, this.battlefieldMarine]);
-        //         this.player1.clickCard(this.p2Base);
-        //         expect(this.player2).toBeActivePlayer()
-        //         expect(this.p1Base.damage).toBe(1);
-        //         expect(this.p2Base.damage).toBe(3);
-        //     });
-        //     it('should damage each base', function () {
-        //         this.player1.clickCard(this.sabineWren);
-        //         expect(this.player1).toBeAbleToSelectExactly([this.p2Base, this.battlefieldMarine]);
-        //         this.player1.clickCard(this.battlefieldMarine);
-        //         expect(this.player2).toBeActivePlayer()
-        //         expect(this.battlefieldMarine.damage).toBe(2);
-        //         expect(this.p1Base.damage).toBe(1);
-        //         expect(this.p2Base.damage).toBe(3);
-        //     });
-        // });
+        describe('Sabine\'s deployed ability', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'action',
+                    player1: {
+                        leader: { card: 'sabine-wren#galvanized-revolutionary', deployed: true }
+                    },
+                    player2: {
+                        groundArena: ['battlefield-marine']
+                    }
+                });
+            });
+
+            it('should damage each base', function () {
+                // attack base, each base damage should be +1
+                this.player1.clickCard(this.sabineWren);
+                expect(this.player1).toBeAbleToSelectExactly([this.p2Base, this.battlefieldMarine]);
+                this.player1.clickCard(this.p2Base);
+
+                expect(this.player2).toBeActivePlayer();
+                expect(this.p1Base.damage).toBe(1);
+                expect(this.p2Base.damage).toBe(3);
+
+                this.sabineWren.exhausted = false;
+                this.player2.passAction();
+
+                // attack another unit, each base should be damaged
+                this.player1.clickCard(this.sabineWren);
+                expect(this.player1).toBeAbleToSelectExactly([this.p2Base, this.battlefieldMarine]);
+                this.player1.clickCard(this.battlefieldMarine);
+
+                expect(this.player2).toBeActivePlayer();
+                expect(this.battlefieldMarine.damage).toBe(2);
+                expect(this.p1Base.damage).toBe(2);
+                expect(this.p2Base.damage).toBe(4);
+            });
+        });
     });
 });
