640b51a3fb484dda7bffe5b9680d279e1b74114c
Valentin Jubert <val_jub@live.fr>
2024-11-03 22:51:58 +0100

    hondo & mando

---
+++ server/game/cards/02_SHD/leaders/TheMandalorianSwornToTheCreed.ts
@@ -0,0 +1,44 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
+
+export default class TheMandalorianSwornToTheCreed extends LeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '9005139831',
+            internalName: 'the-mandalorian#sworn-to-the-creed',
+        };
+    }
+
+    protected override setupLeaderSideAbilities() {
+        this.addTriggeredAbility({
+            title: 'Exhaust this leader',
+            optional: true,
+            when: {
+                onCardPlayed: (event, context) => event.card.controller === context.source.controller && event.card.isUpgrade()
+            },
+            immediateEffect: AbilityHelper.immediateEffects.exhaust(),
+            ifYouDo: {
+                title: 'Exhaust an enemy unit with 4 or less remaining HP',
+                targetResolver: {
+                    cardCondition: (card) => card.isUnit() && card.remainingHp <= 4,
+                    immediateEffect: AbilityHelper.immediateEffects.exhaust()
+                },
+            }
+        });
+    }
+
+    protected override setupLeaderUnitSideAbilities() {
+        this.addTriggeredAbility({
+            title: 'Exhaust an enemy unit with 6 or less remaining HP',
+            when: {
+                onCardPlayed: (event, context) => event.card.controller === context.source.controller && event.card.isUpgrade()
+            },
+            targetResolver: {
+                cardCondition: (card) => card.isUnit() && card.remainingHp <= 6,
+                immediateEffect: AbilityHelper.immediateEffects.exhaust()
+            },
+        });
+    }
+}
+
+TheMandalorianSwornToTheCreed.implemented = true;
